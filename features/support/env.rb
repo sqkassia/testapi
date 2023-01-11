d9de6e4bdbe4e1ae96474d9043c61ebc5b9e5cfb
sqkassia <122458033+sqkassia@users.noreply.github.com>
2023-01-11 11:13:33 -0300

    test public api

---
+++ features/support/env.rb
@@ -0,0 +1,18 @@
+require 'httparty'
+require 'faker'
+require 'pry'
+require_relative 'helper.rb'
+require 'capybara'
+require 'capybara/cucumber'
+require 'report_builder'
+require 'selenium-webdriver'
+require 'ffi'
+
+
+
+Capybara.configure do |config|
+    config.default_driver = :selenium_chrome #roda no navegador
+    config.default_driver = :selenium_chrome_headless #roda com o nageador em background
+end
+
+
